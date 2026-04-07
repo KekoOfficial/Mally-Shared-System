9e7d2355673c57c0c788628f7472b51161d193a0
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-07 05:32:40 -0300

    Actualizar config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = "8572776700:AAGhxY4tTMJbNeqv2zgK6Pz3sHZNbkM69oI"
-CANAL_DESTINO = "@tu_canal_mally"
+CANAL_DESTINO = "-1003301674187"
 PERFILES_TIKTOK = [
     "https://www.tiktok.com/@usuario1",
     "https://www.tiktok.com/@usuario2"
