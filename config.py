c33566a275f407a4028a3a5dcbbbc006a585297e
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-07 05:28:02 -0300

    Crear config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = "TU_BOT_TOKEN"
+CANAL_DESTINO = "@tu_canal_mally"
+PERFILES_TIKTOK = [
+    "https://www.tiktok.com/@usuario1",
+    "https://www.tiktok.com/@usuario2"
+]
+# Configuración de filtro
+DURACION_MIN = 60
+DURACION_MAX = 90
